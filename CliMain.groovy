62925b253ac5ac4ecaabab3e93dfde2aae27cd1a
Jarl Friis <jarl@softace.dk>
2016-04-30 12:59:13 +0200

    Introducing RESTClient

--- CliMain.groovy
+++ CliMain.groovy
@@ -1,27 +1,37 @@
+@Grab('org.codehaus.groovy.modules.http-builder:http-builder:0.7')
+@Grab('oauth.signpost:signpost-core:1.2.1.2')
+@Grab('oauth.signpost:signpost-commonshttp4:1.2.1.2')
+
+import groovyx.net.http.RESTClient
+import static groovyx.net.http.ContentType.*
+
 class CliMain {
     OptionAccessor options
-    String directory
+    LinkedHashMap context = [:]
 
     CliMain(args) {
         CliBuilder cli = new CliBuilder(usage: 'indlever [options] <directory>',
                 header: 'Options:')
         cli.with {
             h longOpt: 'help', 'Usage information'
-            b longOpt: 'base-url', 'Base url, e.g. http://api.geni.skat.dk'
-            p longOpt: 'path', 'Path to resource, e.g. "/Udlån/cvr:<>/2017/indleveringer"'
+            b longOpt: 'base-url', args: 1, 'Base url, e.g. http://api.geni.skat.dk'
+            p longOpt: 'path', args: 1, 'Path to resource, e.g. "/Udlån/cvr:<>/2017/indleveringer"'
         }
         options = cli.parse(args)
         if (options.arguments().size != 1) {
             cli.usage()
-            throw new IllegalArgumentException()
+            throw new IllegalArgumentException("You must provide exactly one directory, not ${options.arguments().size }.")
         }
         if (options.help) {
             cli.usage()
             return
         }
-        directory = options.arguments()[0]
+        context.'base-url' = options.'base-url'?: 'http://api.geni.skat.dk'
+        context.path = options.path
+        context.directory = options.arguments()[0]
     }
 
     def run() {
+        def indlevering = new RESTClient("${context.'base-url'}${context.path}")
     }
 }
--- indlever.groovy
+++ indlever.groovy
@@ -3,6 +3,7 @@ CliMain indlever
 try {
     indlever = new CliMain(args)
 } catch (IllegalArgumentException e) {
+    println e.message
     System.exit 1
 }
 indlever.run()
