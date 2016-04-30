46878ca7de305495b96ee1f089c0e1ae96869479
Jarl Friis <jarl@softace.dk>
2016-04-30 13:55:00 +0200

    Introducing url builder based on arguments
      ./indlever.groovy -n -b http://localhost:4545 -d Udlån -c 19552101 -p 2017 sample_files/2017

--- CliMain.groovy
+++ CliMain.groovy
@@ -14,8 +14,11 @@ class CliMain {
                 header: 'Options:')
         cli.with {
             h longOpt: 'help', 'Usage information'
+            n longOpt: 'dry-run', "Dry run. Do not POST anything"
             b longOpt: 'base-url', args: 1, 'Base url, e.g. http://api.geni.skat.dk'
-            p longOpt: 'path', args: 1, 'Path to resource, e.g. "/Udlån/cvr:<>/2017/indleveringer"'
+            d longOpt: 'domain', args: 1, 'Reporting domain. e.g. "Udlån"', required: true
+            c longOpt: 'cvr', args: 1, 'CVR number', required: true
+            p longOpt: 'period', args: 1, 'Period, e.g. "2017"', required: true
         }
         options = cli.parse(args)
         if (options.arguments().size != 1) {
@@ -26,19 +29,26 @@ class CliMain {
             cli.usage()
             return
         }
+        context.dry = options.'dry-run'
         context.'base-url' = options.'base-url' ?: 'http://api.geni.skat.dk'
-        context.path = options.path
+        context.domain = options.domain
+        context.cvr = options.cvr
+        context.period = options.period
         context.directory = options.arguments()[0]
     }
 
     def run() {
-        RESTClient indlevering = new RESTClient("${context.'base-url'}${context.path}")
+        def url = "${context.'base-url'}/${context.domain}/cvr:${context.cvr}/${context.period}/indleveringer"
+        RESTClient indlevering = new RESTClient(url)
         new File(context.directory).eachFile {
-            indlevering.post(
-                    path: "/${it.name}",
-                    requestContentType : ContentType.APPLICATION_XML,
-                    body: it.getBytes()
-            )
+            println "POST $url/${it.name[0..-5]} content of ${it.name}"
+            if (!context.dry) {
+                indlevering.post(
+                        path: "/${it.name[0..-5]}",
+                        requestContentType: ContentType.APPLICATION_XML,
+                        body: it.getBytes()
+                )
+            }
         }
     }
 }
